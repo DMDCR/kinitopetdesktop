37df7586bc2c62e2e444c3fa4aca41a4a68bfde0
DMDIcefox <111153358+DMDCR@users.noreply.github.com>
2024-03-24 14:02:14 -0500

    Update buildkinito.bat

--- buildkinito.bat
+++ buildkinito.bat
@@ -6,7 +6,6 @@ echo Building Kinito
 pyinstaller -w -F --icon=icon.ico kinito.py
 echo Please check the "dist" folder, if the exe is there, continue
 pause;
-xcopy /s /e /v /i "./audio" "./dist/audio"
 xcopy /s /e /v /i "./models" "./dist/models"
 xcopy /s /e /v /i "./other" "./dist/other"
 echo Done!
