29d91828c5a88fb6e95687831d8b47e2d724b745
DMDIcefox <111153358+DMDCR@users.noreply.github.com>
2024-03-24 14:27:50 -0500

    Update buildkinito.bat

--- buildkinito.bat
+++ buildkinito.bat
@@ -1,9 +1,9 @@
 @echo OFF
-title sigma
+title KinitoPET Desktop Pet Builder
 echo Please continue to build kinito!
 pause;
 echo Building Kinito
-pyinstaller -w -F --icon=icon.ico kinito.py
+pyinstaller -w -F --icon=icon.ico --additional-hooks-dir=hooks --hidden-import=requests kinito.py
 echo Please check the "dist" folder, if the exe is there, continue
 pause;
 xcopy /s /e /v /i "./models" "./dist/models"
