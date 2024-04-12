99142829dd998ebc2a81ca9fad2624870c3810d6
DMDIcefox <111153358+DMDCR@users.noreply.github.com>
2024-04-11 19:40:43 -0500

    some fixes, also installs pyinstaller when you run it

--- buildkinito.bat
+++ buildkinito.bat
@@ -2,8 +2,10 @@
 title KinitoPET Desktop Pet Builder
 echo Please continue to build kinito!
 pause;
+echo Downloading PyInstaller
+pip install pyinstaller
 echo Building Kinito
-pyinstaller -w -F --icon=icon.ico --additional-hooks-dir=hooks --hidden-import=requests kinito.py
+pyinstaller -w -F --clean --icon=icon.ico --additional-hooks-dir=hooks --hidden-import=requests kinito.py
 echo Please check the "dist" folder, if the exe is there, continue
 pause;
 xcopy /s /e /v /i "./models" "./dist/models"
