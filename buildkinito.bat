b5de2918b5a762fee1bdcd3b6548dc8dbea914af
j4ham3z <160253176+j4ham3z@users.noreply.github.com>
2024-04-12 15:22:07 -0500

    Update buildkinito.bat

--- buildkinito.bat
+++ buildkinito.bat
@@ -1,9 +1,16 @@
 @echo OFF
 title KinitoPET Desktop Pet Builder
-echo Please continue to build kinito!
+echo This project requires Python 3.10+, please make sure you have a 3.10+ version of Python, then continue!
+echo Please continue to build Kinito!
 pause;
-echo Downloading PyInstaller
+echo Continuing will Download PyInstaller & the requirements for running the project, are you sure?
+echo Please exit Command Prompt/Terminal if you do not want to continue, if you still want to continue, press any key.
+pause;
 pip install pyinstaller
+pip install pillow
+pip install pygame
+pip install pystray
+pip install requests
 echo Building Kinito
 pyinstaller -w -F --clean --icon=icon.ico --additional-hooks-dir=hooks --hidden-import=requests kinito.py
 echo Please check the "dist" folder, if the exe is there, continue
