dd6377fee66f74d3c5bb75d6bd9701a134fcdade
bloatware <126363200+bwindows10@users.noreply.github.com>
2024-07-13 20:30:11 -0500

    Update discord.cmd

--- cmd/discord.cmd
+++ cmd/discord.cmd
@@ -1,2 +1,2 @@
-echo batch scripts made by bloatware - contact me on discord at @bwindows10
+echo batch scripts made by bloatware - contact him on discord at @bwindows10
 start C:\Users\%USERNAME%\AppData\Local\Discord\Update.exe
